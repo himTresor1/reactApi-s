369e86e774e83aa7106ee4e76b83b54e61c32eb6
himTresor1 <amizeropeace@gmail.com>
2023-04-24 21:23:00 +0200

    Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import googleMapReact from 'google-map-react';
+import {Paper , Typography, useMediaQuery}  from "@material-ui/core"
+import { LocationOnOutlinedIcon } from '@material-ui/icons/LocationOutlined';
+import Rating  from "@material-ui/lab"
+import useStyles  from "./styles"
+
  const Map = () => {
+     const classes= useStyles();
+     const isMobile= useMediaQuery('(min')
 
     return(
 
