e89be5fc45c5c8062509871c4fbde9dd38f51874
himTresor1 <amizeropeace@gmail.com>
2023-04-24 21:27:49 +0200

    Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import googleMapReact from 'google-map-react';
-import {Paper , Typography, useMediaQuery}  from "@material-ui/core"
-import { LocationOnOutlinedIcon } from '@material-ui/icons/LocationOutlined';
-import Rating  from "@material-ui/lab"
-import useStyles  from "./styles"
+import React from "react";
+import googleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOutlined";
+import Rating from "@material-ui/lab";
+import useStyles from "./styles";
 
- const Map = () => {
-     const classes= useStyles();
-     const isMobile= useMediaQuery('(min')
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width= 600px)");
+  const coordinates = { lat: 0, lng: 0 };
 
-    return(
+  return (
+    <div className={classes.mapContainer}>
+      <googleMapReact
+        bootstrapUrlKeys={{ key: "" }}
+        defaultCneter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></googleMapReact>
+    </div>
+  );
+};
 
-        <h3>Map</h3>
-
-    );
-
- }
-
- export default Map;
+export default Map;
